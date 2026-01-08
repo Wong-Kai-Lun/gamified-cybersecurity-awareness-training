1a1492fa501d0b8b5bf54a0915831fc2b2b551fb
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2026-01-08 18:27:17 +0800

    Create Game Main Menu

---
+++ godot/main_menu/main_menu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_new_game_pressed() -> void:
+	print("New Game Button pressed.")
+
+
+func _on_continue_pressed() -> void:
+	print("Continue Button pressed.")
+
+
+func _on_settings_pressed() -> void:
+	print("Settings Button pressed.")
+
+
+func _on_quit_game_pressed() -> void:
+	print("Quit Game Button pressed.")
+	get_tree().quit()
