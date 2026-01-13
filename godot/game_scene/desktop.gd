6d39e56cd3d5aabc9ab37eb139ae3b9db33f2001
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2026-01-13 14:08:29 +0800

    Update Desktop Scene

---
+++ godot/game_scene/desktop.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var settings_scene: PackedScene = preload("res://game_scene/programs/settings.tscn")
+@onready var settings_spawn = $CanvasLayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_settings_pressed() -> void:
+	var settings = settings_scene.instantiate()
+	settings_spawn.add_child(settings)
