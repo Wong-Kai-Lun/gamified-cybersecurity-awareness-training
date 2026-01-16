1204c1be62203b377aed981c066ee346691b643f
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2026-01-16 10:34:40 +0800

    Update IncomingEmail to be clickable

--- godot/game/main_game/programs/email_client/incoming_email.gd
+++ godot/game/main_game/programs/email_client/incoming_email.gd
@@ -1,8 +1,8 @@
 extends Control
 
-@onready var sender_label: RichTextLabel = $PanelContainer/MarginContainer/VBoxContainer/SenderNameRTL
-@onready var subject_label: RichTextLabel = $PanelContainer/MarginContainer/VBoxContainer/SubjectLineRTL
-@onready var body_preview_label: RichTextLabel = $PanelContainer/MarginContainer/VBoxContainer/BodyPreviewRTL
+@onready var sender_label: RichTextLabel = $PanelContainer/Open/MarginContainer/VBoxContainer/SenderNameRTL
+@onready var subject_label: Label = $PanelContainer/Open/MarginContainer/VBoxContainer/SubjectLbl
+@onready var body_preview_label: Label = $PanelContainer/Open/MarginContainer/VBoxContainer/BodyPreviewLbl
 
 var email_data: EmailData
 
@@ -12,3 +12,7 @@ func setup(data: EmailData) -> void:
 	sender_label.text = "[b]" + data.sender_name
 	subject_label.text = data.subject
 	body_preview_label.text = data.email_body
+
+
+func _on_open_pressed() -> void:
+	pass # Replace with function body.
