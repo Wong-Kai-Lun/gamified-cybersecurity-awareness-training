39a76f53e2a2f8c8be5025dfde9799fac5a4a8aa
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2026-01-23 17:44:45 +0800

    Restructure Game Data
    - Created more folders for different data.

--- godot/data/email_data.gd
+++ godot/data/email_data.gd
@@ -1,11 +1,14 @@
 extends Resource
 class_name EmailData
 
-@export var email_type: String
+enum EmailType { MESSAGE, INBOUND, OUTBOUND }
+
+@export var email_type: EmailType = EmailType.MESSAGE
 @export var sender_name: String
 @export var sender_address: String
 @export var to_address: String
 @export var cc_address: String
 @export var subject: String
 @export var email_body: String
-@export var flags: Array[String] = []
+@export var attached_files: Array[FileData] = []
+@export var score: float
---
+++ godot/data/file_data.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name FileData
+
+enum FileContext { INBOUND, INVENTORY, TRASH }
+enum FileType { LEGIT, MALWARE, ADWARE, RANSOMWARE }
+
+@export var file_id: String
+@export var file_texture: Texture2D
+@export var file_name: String
+@export var file_context: FileContext = FileContext.INBOUND
+@export var file_type: FileType = FileType.LEGIT 
