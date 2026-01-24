3b0eb930f8ec677c528f32fb287b2e5a51f2d203
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2026-01-24 17:10:06 +0800

    Create GameManager.gd
    - Created GameManager.gd and set it to autoload to handle player actions and data changes.

---
+++ godot/game/game_manager.gd
@@ -0,0 +1,15 @@
+extends Node
+class_name GameManager
+
+signal inventory_changed(inventory: Array[FileData])
+
+var _inventory: Array[FileData] = []
+
+func _ready():
+	print("GameManager is online!")
+
+func add_file_to_inventory(file: FileData) -> void:
+	var new_file := file.duplicate()
+	new_file.file_context = FileData.FileContext.INVENTORY
+	_inventory.append(new_file)
+	inventory_changed.emit(_inventory.duplicate())
