f7b40ac2cf2cfcb5152e1915715ae32cc959eae4
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2026-02-03 15:26:56 +0800

    Add Button Status on Player Input
    - if username or email LineEdit input fields are empty, disable the Start/Play button.

--- godot/game/main_menu/new_player_window.gd
+++ godot/game/main_menu/new_player_window.gd
@@ -6,11 +6,24 @@ signal new_player_window_closed
 
 @onready var username_input: LineEdit = $WindowPanel/Structure/Content/MarginContainer/VBoxContainer/UsernameVBox/UsernameInput
 @onready var email_input: LineEdit = $WindowPanel/Structure/Content/MarginContainer/VBoxContainer/EmailVBox/HBoxContainer/EmailInput
-
+@onready var start_button: Button = $WindowPanel/Structure/Content/MarginContainer/VBoxContainer/StartButton
 
 func _ready() -> void:
 	super._ready()
+	_on_line_edit_text_changed(username_input.text)
+	_on_line_edit_text_changed(email_input.text)
+	username_input.text_changed.connect(_on_line_edit_text_changed)
+	email_input.text_changed.connect(_on_line_edit_text_changed)
 
+
+func _on_line_edit_text_changed(new_text: String) -> void:
+	var is_username_empty = username_input.text.strip_edges().is_empty()
+	var is_email_empty = email_input.text.strip_edges().is_empty()
+	start_button.disabled = is_username_empty or is_email_empty
+	
+	print("is_username_empty: ", is_username_empty)
+	print("is_email_empty: ", is_email_empty)
+	#insert regex logic here?
 
 func on_close_requested() -> void:
 	new_player_window_closed.emit()
