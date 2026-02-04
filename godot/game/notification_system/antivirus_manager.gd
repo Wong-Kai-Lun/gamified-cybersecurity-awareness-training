4f2f9a9dcafb6e10f7d27673da7158a21ffa369f
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2026-02-04 12:45:07 +0800

    Add Feature: Antivirus Auto Scan
    - now antivirus automatically scans the player's inventory and displays any found threats. (every 8 seconds for now)
    - will add story building to why only one file is shown at a time (cheapest subscription?)

--- godot/game/notification_system/antivirus_manager.gd
+++ godot/game/notification_system/antivirus_manager.gd
@@ -1,11 +1,28 @@
 extends Node
 class_name AntivirusManager
 
-signal malware_found
+signal threat_found(file_name: String)
+
+@onready var scan_timer: Timer = Timer.new()
+
+func _ready() -> void:
+	print("Antivirus online!")
+	add_child(scan_timer)
+	scan_timer.wait_time = 8.0
+	scan_timer.one_shot = false
+	scan_timer.timeout.connect(scan_player_inventory)
+	scan_timer.start()
+
 
 func scan_player_inventory() -> void:
 	var player_inventory = GameManagerInstance.get_player_inventory()
+	var has_malware = false
+	var caught_file = ""
 	
 	for file in player_inventory:
-		print(file.file_name)
-		print(file.FileType)
+		if file.file_type != FileData.FileType.LEGIT:
+			has_malware = true
+			caught_file = file.file_name
+	
+	if has_malware:
+		threat_found.emit(caught_file)
--- godot/game/notification_system/notification_manager.gd
+++ godot/game/notification_system/notification_manager.gd
@@ -5,13 +5,16 @@ signal request_notification(data)
 
 func _ready() -> void:
 	GameManagerInstance.inventory_full.connect(_notify_inventory_full)
+	AntivirusManagerInstance.threat_found.connect(_notify_malware_detected)
 
 func register_file(file: File) -> void:
 	file.file_downloaded.connect(_notify_file_downloaded)
 
 func _notify_file_downloaded(file_data: FileData) -> void:
 	request_notification.emit({ "title": "File Downloaded", "body": file_data.file_name })
-	AntivirusManagerInstance.scan_player_inventory()
 
 func _notify_inventory_full() -> void:
 	request_notification.emit({ "title": "No more space", "body": "Please remove some files." })
+
+func _notify_malware_detected(file_name: String) -> void:
+	request_notification.emit({ "title": "Malware Detected!", "body": file_name })
