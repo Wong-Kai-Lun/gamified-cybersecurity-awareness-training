3c34a0fa50ea3007828fe7f4da7abe9b33cc1498
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2026-02-04 13:41:04 +0800

    Set Notification Feed to not block mouse input
    - Set Notification Feed to ignore mouse input in order to not block dragging windows like Inventory.

--- godot/game/notification_system/antivirus_manager.gd
+++ godot/game/notification_system/antivirus_manager.gd
@@ -5,10 +5,11 @@ signal threat_found(file_name: String)
 
 @onready var scan_timer: Timer = Timer.new()
 
+
 func _ready() -> void:
 	print("Antivirus online!")
 	add_child(scan_timer)
-	scan_timer.wait_time = 8.0
+	scan_timer.wait_time = 16.0
 	scan_timer.one_shot = false
 	scan_timer.timeout.connect(scan_player_inventory)
 	scan_timer.start()
