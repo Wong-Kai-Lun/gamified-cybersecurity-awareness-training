67bb9c11c1753b4292b015a1311dca20152731a9
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2026-02-09 10:54:31 +0800

    Create AdWindow for Adware attack

---
+++ godot/game/main_game/programs/events/ad_window.gd
@@ -0,0 +1,13 @@
+extends BaseWindow
+class_name AdWindow
+
+@export var possible_textures: Array[Texture2D]
+@onready var ad_texture_rect: TextureRect = $WindowPanel/Structure/Content/PanelContainer/AdTextureRect
+
+func _ready() -> void:
+	super._ready()
+	setup()
+	
+
+func setup() -> void:
+	ad_texture_rect.texture = possible_textures.pick_random()
