b83f8b8b883d3ea03cddb8dd91c46771f5a553a1
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2026-03-04 15:14:06 +0800

    Level Design: Monday Update

--- godot/game/autoload/email_database.gd
+++ godot/game/autoload/email_database.gd
@@ -4,16 +4,15 @@ class_name EmailDatabase
 const _email_paths_by_id := {
 	# Monday
 	"1_1_onboarding": "res://data/email/monday/1_1_onboarding.tres",
-	"1_2_onboarding2": "res://data/email/monday/1_2_onboarding2.tres",
-	"1_3_submit_po": "res://data/email/monday/1_3_submit_po.tres",
-	"1_4_littlegenius_po": "res://data/email/monday/1_4_littlegenius_po.tres",
-	"1_5_antivirus_introduction": "res://data/email/monday/1_5_antivirus_introduction.tres",
-	"1_6_antivirus_activated": "res://data/email/monday/1_6_antivirus_activated.tres",
-	"1_7_trippo_ad": "res://data/email/monday/1_7_trippo_ad.tres",
-	"1_8_happyminimarket_po": "res://data/email/monday/1_8_happyminimarket_po.tres",
-	"1_9_cybernewsdaily": "res://data/email/monday/1_9_cybernewsdaily.tres",
-	"1_10_fake_po": "res://data/email/monday/1_10_fake_po.tres",
-	"1_11_printpro_po": "res://data/email/monday/1_11_printpro_po.tres",
+	"1_2_submit_po": "res://data/email/monday/1_2_submit_po.tres",
+	"1_3_antivirus_introduction": "res://data/email/monday/1_3_antivirus_introduction.tres",
+	"1_4_antivirus_activated": "res://data/email/monday/1_4_antivirus_activated.tres",
+	"1_5_littlegenius_po": "res://data/email/monday/1_5_littlegenius_po.tres",
+	"1_6_trippo_ad": "res://data/email/monday/1_6_trippo_ad.tres",
+	"1_7_happyminimarket_po": "res://data/email/monday/1_7_happyminimarket_po.tres",
+	"1_8_cybernewsdaily": "res://data/email/monday/1_8_cybernewsdaily.tres",
+	"1_9_fake_po": "res://data/email/monday/1_9_fake_po.tres",
+	"1_10_printpro_po": "res://data/email/monday/1_10_printpro_po.tres",
 	
 	# Tuesday
 }
